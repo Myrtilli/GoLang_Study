abf1f7b5f2ae2e5818b92d47aff34c64336409de
Kate Myrtilli <kopt7877@gmail.com>
2025-08-10 13:38:54 +0300

    changes were made for last task

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "strings"
+)
+
+type Vertex struct {
+	url1 string
+	url2 string
+    url3 string
+}
 
 func main() {
-    fmt.Println("Client is typing")
+    url := Vertex{"google.com", "youtube.com", "moodle.com"}
+    join(url)
+}
+
+func join(v Vertex) {
+    urls := []string{v.url1, v.url2, v.url3}
+    for _, u := range urls {
+        fullURL := strings.Join([]string{"http://", u}, "")
+        fmt.Println(fullURL)
+    }
 }
